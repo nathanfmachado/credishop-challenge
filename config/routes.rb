95ec4d57046841428753f0b1d96584a733c76cec
Nathan Fabiano Machado <nathanfabiano@hotmail.com>
2022-01-31 08:17:27 -0300

    Proponent by salary Report created

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,18 @@ require 'sidekiq/web'
 
 Rails.application.routes.draw do
   mount Sidekiq::Web => '/sidekiq'
+  devise_for :users
 
   resources :proponents do
     collection do
       get :calculate_inss_discount
     end
   end
-  devise_for :users
+
+  resources :reports, only: :index do
+    collection do
+      get :proponent_by_salary
+    end
+  end
+  
 end
