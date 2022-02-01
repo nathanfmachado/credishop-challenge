5a93733db77af8f0451c211f60554c0d3bf48eda
Nathan Fabiano Machado <nathanfabiano@hotmail.com>
2022-01-31 22:30:04 -0300

    Reports controller and view refactored - Business rules transferred from controller to model - Line chart added

--- app/models/proponent.rb
+++ app/models/proponent.rb
@@ -8,4 +8,28 @@ class Proponent < ApplicationRecord
   validates :name, :birth_date, :salary, :inss_discount, presence: true
 
   paginates_per 5
+
+  def self.proponent_by_salary
+    min = max = nil
+    cases = SALARY_RANGES.map do |r|
+      min = [r[:min], min || r[:min]].min
+      max = [r[:max], max || r[:max]].max
+      "when salary between #{r[:min]} and #{r[:max]} then 'De R$ #{r[:min]} à R$ #{r[:max]}'"
+    end
+
+    salary_ranges = Proponent.select("count(*) as n, case #{cases.join(' ')} end as salary_range").where(salary: min..max).group("salary_range").all
+
+    return Hash[salary_ranges.map { |r| [r.salary_range, r.n] }]
+  end
+
+  private
+
+    SALARY_RANGES = [
+      { min: 0, max: 1045 },
+      { min: 1045.001, max: 2089.6 },
+      { min: 2089.601, max: 3134.4 },
+      { min: 3134.401, max: 6101.06 },
+      { min: 6101.061, max: 999_999 }
+    ]
+
 end
